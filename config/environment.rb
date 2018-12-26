b83c7219bbddc8c935468f831f2d133b2fc2daab
Vince Andrew Pimentel <contact@vincep.me>
2018-12-26 01:33:41 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -4,15 +4,12 @@ require 'yaml/store'
 require 'ostruct'
 require 'date'
 
-
 require 'bundler/setup'
 Bundler.require
-
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/artists.sqlite"
 )
-
 
 require_relative "../artist.rb"
